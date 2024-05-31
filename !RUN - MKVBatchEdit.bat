83a4bdcb34cb3db790c84838e6985492fe939b21
VioletSn <pandaman23@bigpond.com>
2024-05-31 15:19:04 +1000

    Created !RUN - MKVBatchEdit.bat
    Made a .bat file which runs the main script so that the mains script can stay in one location.

---
+++ !RUN - MKVBatchEdit.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+SET powershell_script_path="D:\Documents\GitHub\MKV_Batch_Editor\!Master.ps1"
+"C:\Program Files\PowerShell\7\pwsh.exe" -ExecutionPolicy Bypass -NoProfile -File "%powershell_script_path%"
